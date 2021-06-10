d41deef27f8541be3d3aff6d1c67aa760a5aa5fe
Gustavo Barretto <gustavobarretto@gmail.com>
2021-06-10 00:27:43 -0300

    adicionando atividades de programacao imperativa 1

---
+++ controleDeFluxo/006.js
@@ -0,0 +1,7 @@
+// EXERCÍCIO: Igualdade, if e else
+let linguagem = 'jabuticaba'
+if (linguagem == 'javascript') {
+    console.log('Estou aprendendo!')
+} else {
+    console.log('Aprenderei mais tarde...')
+}
